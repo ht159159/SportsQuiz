425523c050543334a9b87221dd5a85dcdc809958
Sheep <ht159159@gmail.com>
2021-01-09 17:37:16 +0800

    first commit11

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
       }
     }
   },
-  publicPath: isGoogle=="true" ? 'https://cdn.juyuanhe.com.cn/crazyguess/' : './',
+  // publicPath: isGoogle=="true" ? 'https://cdn.juyuanhe.com.cn/crazyguess/' : './',
   productionSourceMap:false,//如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
   lintOnSave: false,
   chainWebpack: config => {
